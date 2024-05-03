6aa4354ab1b4b1f1aa42fb5f50b2cf08b46b355c
yuliaLavr <160929826+yuliaLavr@users.noreply.github.com>
2024-05-03 12:07:56 +0300

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -3,21 +3,38 @@ public class Room {
     int fl;
     String type;
 
-    public Room(int num, int fl, String type) {
-        this.num = num;
+    public Room(int fl, String type) {
         this.fl = fl;
         this.type = type;
     }
 
-    public int getNum() {
+    public Room() {
+    }
+
+    public Room(int fl) {
+        this.fl = fl;
+    }
+
+    int  number() {
+        num=(int)(Math.random()*200+100);
         return num;
+    }
+
+    String typeOfRoom(int num){
+        if (num == 1) {
+            type ="одномісний";
+        }
+        else if (num==2){
+            type ="двомісний";
+        }
+        else{
+            type="двокімнатний";
+        }
+        return type;
     }
 
     public int getFl() {
         return fl;
     }
 
-    public String getType() {
-        return type;
-    }
 }
